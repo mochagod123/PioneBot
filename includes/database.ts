b0df38d87a1a62cea717f1184494d8152bd10d22
renorari <renorari@renorari.net>
2024-07-20 19:56:15 +0900

    ✨ add join message

--- includes/database.ts
+++ includes/database.ts
@@ -5,6 +5,14 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
+interface ServerJoinSettings {
+    serverId: string;
+    channelId: string;
+    imageTemplate: number;
+    joinMessage: string;
+    bottomMessage: string;
+}
+
 dotenv.config();
 const connection = mysql.createPool({
     host: process.env.DB_HOST,
@@ -20,12 +28,20 @@ function getConnection() {
     return connection;
 }
 
-function getServerJoinSettings(serverId: string): Promise<mysql.RowDataPacket[]> {
-    return connection.query("SELECT * FROM server_join_settings WHERE server_id = ?", [serverId]) as Promise<mysql.RowDataPacket[]>;
-}
-
 function setServerJoinSettings(serverId: string, channelId: string, imageTemplate: number, joinMessage: string, bottomMessage:string): Promise<mysql.RowDataPacket[]> {
     return connection.query("INSERT INTO server_join_settings (server_id, channel_id, image_template, join_message, bottom_message) VALUES (?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE channel_id = ?, image_template = ?, join_message = ?, bottom_message = ?", [serverId, channelId, imageTemplate, joinMessage, bottomMessage, channelId, imageTemplate, joinMessage, bottomMessage]) as Promise<mysql.RowDataPacket[]>;
+}
+
+async function getServerJoinSettings(serverId: string): Promise<ServerJoinSettings | null> {
+    const [rows] = await connection.query("SELECT * FROM server_join_settings WHERE server_id = ?", [serverId]) as mysql.RowDataPacket[];
+    if (rows.length === 0) return null;
+    return {
+        serverId: rows[0].server_id,
+        channelId: rows[0].channel_id,
+        imageTemplate: rows[0].image_template,
+        joinMessage: rows[0].join_message,
+        bottomMessage: rows[0].bottom_message
+    };
 }
 
 export { getConnection, getServerJoinSettings, setServerJoinSettings };
--- index.ts
+++ index.ts
@@ -2,14 +2,36 @@
     PioneBOT Main
 */
 
-import { Client, REST, Routes, GatewayIntentBits, SlashCommandBuilder, ActivityType, EmbedBuilder, Colors, ChannelType } from "discord.js";
+import { Client, REST, Routes, GatewayIntentBits, SlashCommandBuilder, ActivityType, EmbedBuilder, Colors, ChannelType, PermissionFlagsBits } from "discord.js";
 import dotenv from "dotenv";
 import * as db from "./includes/database";
 import * as joinImage from "./includes/create-join-image";
 
 dotenv.config();
 const token = process.env.DISCORD_TOKEN as string;
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+    intents: [
+        GatewayIntentBits.AutoModerationConfiguration,
+        GatewayIntentBits.AutoModerationExecution,
+        GatewayIntentBits.DirectMessagePolls,
+        GatewayIntentBits.DirectMessageReactions,
+        GatewayIntentBits.DirectMessageTyping,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.GuildEmojisAndStickers,
+        GatewayIntentBits.GuildIntegrations,
+        GatewayIntentBits.GuildInvites,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessagePolls,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessageTyping,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildModeration,
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildWebhooks,
+        GatewayIntentBits.MessageContent
+    ]
+});
 const rest = new REST({ version: "10" }).setToken(token);
 const commands = [
     new SlashCommandBuilder()
@@ -17,20 +39,27 @@ const commands = [
         .setDescription("Replies with Pong!")
         .setDescriptionLocalization("ja", "Pong! と返信します。"),
     new SlashCommandBuilder()
-        .setName("settings")
+        .setName("server-settings")
         .setDescription("PioneBOT's settings")
         .setDescriptionLocalization("ja", "PioneBOTの設定")
+        .setDMPermission(false)
+        .setDefaultMemberPermissions(
+            PermissionFlagsBits.ManageGuild +
+            PermissionFlagsBits.ManageChannels +
+            PermissionFlagsBits.SendMessages
+        )
         .addSubcommand(subcommand =>
             subcommand
                 .setName("join")
                 .setDescription("Join message settings")
                 .setDescriptionLocalization("ja", "入室メッセージの設定")
-                .addStringOption(option =>
+                .addChannelOption(option =>
                     option
                         .setName("channel")
                         .setDescription("Channel to send join message")
                         .setDescriptionLocalization("ja", "入室メッセージを送信するチャンネル")
                         .setRequired(true)
+                        .addChannelTypes(ChannelType.GuildText)
                 )
                 .addIntegerOption(option =>
                     option
@@ -47,14 +76,14 @@ const commands = [
                 )
                 .addStringOption(option =>
                     option
-                        .setName("join_message")
+                        .setName("join-message")
                         .setDescription("Join message")
                         .setDescriptionLocalization("ja", "入室メッセージ")
                         .setRequired(true)
                 )
                 .addStringOption(option =>
                     option
-                        .setName("bottom_message")
+                        .setName("bottom-message")
                         .setDescription("Bottom message")
                         .setDescriptionLocalization("ja", "下部メッセージ")
                         .setRequired(true)
@@ -101,36 +130,91 @@ client.on("interactionCreate", async interaction => {
                     .setColor(Colors.Purple)
             ]
         });
+    } else if (interaction.commandName === "server-settings") {
+        if (interaction.guild === null) return;
+
+        if (interaction.options.getSubcommand() === "join") {
+            const channelId = interaction.options.getChannel("channel")?.id as string;
+            const imageTemplate = interaction.options.getInteger("template") as number;
+            const joinMessage = interaction.options.getString("join-message")?.replace(/\\n/g, "\n") as string;
+            const bottomMessage = interaction.options.getString("bottom-message") as string;
+
+            await db.setServerJoinSettings(interaction.guildId as string, channelId, imageTemplate, joinMessage, bottomMessage);
+
+            await interaction.reply({
+                "content": "設定を保存しました!\n以下が設定内容とそのプレビューです。",
+                "embeds": [
+                    new EmbedBuilder()
+                        .setTitle("参加メッセージ設定")
+                        .addFields([
+                            {
+                                name: "Channel",
+                                value: `<#${channelId}>`
+                            },
+                            {
+                                name: "Image Template",
+                                value: imageTemplate.toString() + "番"
+                            },
+                            {
+                                name: "Join Message",
+                                value: joinMessage.replace(/{user}/g, "(ユーザー名)")
+                            },
+                            {
+                                name: "Bottom Message",
+                                value: bottomMessage
+                            }
+                        ])
+                        .setColor(Colors.Green)
+                        .setImage(`attachment://join-${interaction.user.id}.png`)
+                ],
+                "files": [
+                    {
+                        "attachment": await joinImage.createJoinImage(
+                            interaction.user.avatarURL({
+                                "extension": "png",
+                                "size": 256
+                            }) as string,
+                            joinMessage.replace(/{user}/g, interaction.guild.members.cache.get(interaction.user.id)?.displayName as string),
+                            bottomMessage,
+                            imageTemplate
+                        ),
+                        "name": `join-${interaction.user.id}.png`
+                    }
+                ]
+            });
+        }
     }
 });
 
 // Join message
 client.on("guildMemberAdd", async member => {
     const serverId = member.guild.id;
-    const [rows] = await db.getServerJoinSettings(serverId);
-    if (rows.length === 0) return;
+    const settings = await db.getServerJoinSettings(serverId);
+    if (!settings) return;
 
-    const channelId = rows[0].channel_id;
-    const imageTemplate = rows[0].image_template;
-    const joinMessage = rows[0].join_message.replace("{user}", member.displayName);
-    const bottomMessage = rows[0].bottom_message.replace("{user}", member.displayName);
+    const channelId = settings.channelId;
+    const imageTemplate = settings.imageTemplate;
+    const joinMessage = settings.joinMessage.replace(/\{user\}/g, member.displayName);
+    const bottomMessage = settings.bottomMessage.replace(/\{user\}/g, member.displayName);
 
-    const channel = member.guild.channels.cache.get(channelId);
+    const channel = await member.guild.channels.fetch(channelId);
     if (!channel || channel.type != ChannelType.GuildText) return;
+    const avatar = member.user.avatarURL({
+        "extension": "png",
+        "size": 256
+    }) ?? member.user.defaultAvatarURL;
+
+    const image = await joinImage.createJoinImage(
+        avatar,
+        joinMessage,
+        bottomMessage,
+        imageTemplate
+    );
 
     await channel.send({
-        "content": `${joinMessage}\n${bottomMessage}`,
         "files": [
             {
-                "attachment": await joinImage.createJoinImage(
-                    member.avatarURL({
-                        "extension": "png",
-                        "size": 256
-                    }) as string,
-                    joinMessage,
-                    bottomMessage,
-                    imageTemplate
-                ),
+                "attachment": image,
                 "name": `join-${member.id}.png`
             }
         ]
