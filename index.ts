97eeb35c69e411c15bf276f6883a2982041a2e49
renorari <renorari@renorari.net>
2024-07-20 14:31:05 +0900

    ✨ basic discord bot

--- index.ts
+++ index.ts
@@ -1,3 +1,32 @@
 /*
     PioneBOT Main
 */
+
+import { Client, REST, Routes, GatewayIntentBits, SlashCommandBuilder } from "discord.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+const token = process.env.DISCORD_TOKEN as string;
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const rest = new REST({ version: "10" }).setToken(token);
+const commands = [
+    new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("Replies with Pong!")
+];
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user?.tag}!`);
+
+    rest.put(Routes.applicationCommands(client.application?.id as string), { body: commands });
+});
+
+client.on("interactionCreate", async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === "ping") {
+        await interaction.reply("Pong!");
+    }
+});
+
+client.login(token);
